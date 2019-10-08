3380738f551d52a874ec047e9ff3943d801017db
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-08 15:12:53 +0600

    basic style

--- src/ParticleEffectButton.svelte
+++ src/ParticleEffectButton.svelte
@@ -1,3 +1,32 @@
 <script>
 
- 
+ <script>
+    
+ </script>
+
+
+ <style>
+.particles {
+  position: relative;
+  display: inline-block;
+}
+
+.wrapper {
+  position: relative;
+  display: inline-block;
+  overflow: hidden;
+}
+
+.content:focus,
+.content > *:focus {
+  outline: none;
+}
+
+.canvas {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate3d(-50%, -50%, 0);
+  pointer-events: none;
+}
+ </style>
