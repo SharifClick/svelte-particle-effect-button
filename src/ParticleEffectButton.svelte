4d3009aa16511f539c1962c3207fd6055dc5ec2e
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-08 15:28:54 +0600

    initial props

--- src/ParticleEffectButton.svelte
+++ src/ParticleEffectButton.svelte
@@ -1,32 +1,38 @@
 <script>
 
- <script>
+    let hidden = false;
+    let duration = 1000;
+    let easing = 'easeInOutCubic';
+    let type = 'circle';
+    let style = 'fill';
+    let direction = 'left';
+    let canvasPadding = 150;
     
  </script>
 
 
  <style>
-.particles {
-  position: relative;
-  display: inline-block;
-}
+  .particles {
+    position: relative;
+    display: inline-block;
+  }
 
-.wrapper {
-  position: relative;
-  display: inline-block;
-  overflow: hidden;
-}
+  .wrapper {
+    position: relative;
+    display: inline-block;
+    overflow: hidden;
+  }
 
-.content:focus,
-.content > *:focus {
-  outline: none;
-}
+  .content:focus,
+  .content > *:focus {
+    outline: none;
+  }
 
-.canvas {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate3d(-50%, -50%, 0);
-  pointer-events: none;
-}
+  .canvas {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    pointer-events: none;
+  }
  </style>
