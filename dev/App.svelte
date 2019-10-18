1101d5fb6f2308db1624ea230d21f4f2455029ae
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-18 18:50:40 +0600

    dummy props

--- dev/App.svelte
+++ dev/App.svelte
@@ -14,6 +14,22 @@
       animating = false
   }
 
+    let background ='linear-gradient(120deg,#7f54e6,#3F51B5)',
+    text = 'Upload',
+    buttonStyles = {
+      background: '#f3f3f3',
+      color: '#3c2e9e',
+      padding: '1.8rem 4rem'
+    },
+    buttonOptions = {
+      color: '#f3f3f3',
+      type: 'triangle',
+      easing: 'easeOutQuart',
+      size: 6,
+      particlesAmountCoefficient: 4,
+      oscillationCoefficient: 2
+    }
+
 </script>
 
 <style>
