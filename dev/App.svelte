af9cdc3eadefa8e2575fe8624c4042dbe6749491
Sharif Ahmed <me.sharifahmed@gmail.com>
2019-10-18 18:50:58 +0600

    bug fix

--- dev/App.svelte
+++ dev/App.svelte
@@ -34,28 +34,28 @@
 
 <style>
   .container{
-    position: 'relative';
-    display: 'flex';
-    flex-direction: 'column';
-    justify-content: 'center';
-    align-items: 'center';
-    width: '46vmax';
-    height: '46vmax';
-    margin: '2vmax 2vmax 0 0';
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 46vmax;
+    height: 46vmax;
+    margin: 2vmax 2vmax 0 0;
     background: grey;
   }
   button.reset{
-    position: 'absolute';
-    top: '1em';
-    right: '1em';
-    background: '#32bafa';
-    color: '#fff';
-    border: '0';
+    position: absolute;
+    top: 1em;
+    right: 1em;
+    background: #32bafa;
+    color: #fff;
+    border: 0;
     border-radius: 4;
-    font-size: '1em';
-    padding: '0.7em 1.2em';
-    cursor: 'pointer';
-    outline: 'none';
+    font-size: 1em;
+    padding: 0.7em 1.2em;
+    cursor: pointer;
+    outline: none;
   }
 
   button{
